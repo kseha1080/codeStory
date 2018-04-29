b9488b68d43d61810a362c68060346365cc2e91c
Denis Rodriguez-Ramos <ventolucci@hotmail.com>
2018-04-28 21:34:07 -0400

    update

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//passport config
+require('./config/passport')(passport)
+
+//load router
+const auth = require('./routes/auth')
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('hello world')
-})
+});
+
+//use routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
